7e3cfb862e15aae98dae188e344346d37fab16e8
allausova <allausova0109@gmail.com>
2024-06-22 22:47:11 +0200

    new commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
+<<<<<<< HEAD
       The country of Oceanus has sought FishEye International’s help in identifying companies possibly engaged in illegal, unreported, and unregulated (IUU) fishing. 
       Using visual analytics, can you help FishEye identify companies that may be engaged in illegal fishing?
 
@@ -18,6 +19,17 @@ defineProps({
        for more information and to download the data.
     
       </h3>
+=======
+      
+      The country of Oceanus has sought FishEye International’s help in identifying companies possibly engaged in illegal, unreported, and unregulated (IUU) fishing. Using visual analytics, can you help FishEye identify companies that may be engaged in illegal fishing?
+      
+      Please visit <a href="https://vast-challenge.github.io/2023/MC2.html" target="_blank" rel="noopener">VAST Challenge 2023: Mini-Challenge 2</a>  for more information and to download the data.
+      
+      Developed using
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+>>>>>>> b1ac62d (new commit)
   </div>
 </template>
 
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,22 +12,39 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
+<<<<<<< HEAD
     <template #heading>First</template>
 
     Use visual analytics to identify temporal patterns for individual entities and between entities in the knowledge graph FishEye created from trade records. 
     Categorize the types of business relationship patterns you find. 
     Limit your response to 600 words and 6 images.
+=======
+    <template #heading>First question</template>
+
+    Use visual analytics to identify temporal patterns for individual entities and between entities in the knowledge graph FishEye created from trade records. 
+    Categorize the types of business relationship patterns you find. Limit your response to 600 words and 6 images.
+    <!-- <a href="https://vuejs.org/" target="_blank" rel="noopener">газозууу</a>"-->
+>>>>>>> b1ac62d (new commit)
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <ToolingIcon />
     </template>
+<<<<<<< HEAD
     <template #heading>Second</template>
     Evaluate the sets of predicted knowledge graph links FishEye has provided using visual analytics. 
     Which sets are most reliable for completing the graph? 
     Limit your response to 600 words and 6 images.
     <!--This project is served and bundled with
+=======
+    <template #heading>Second question</template>
+
+    Evaluate the sets of predicted knowledge graph links FishEye has provided using visual analytics. 
+    Which sets are most reliable for completing the graph? 
+    600 words and 6 graphs
+     <!-- 
+>>>>>>> b1ac62d (new commit)
     <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
     recommended IDE setup is
     <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
@@ -37,26 +54,45 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
       >Cypress Component Testing</a
     >.
-
     <br />
 
+<<<<<<< HEAD
     More instructions are available in <code>README.md</code>.-->
+=======
+    More instructions are available in <code>README.md</code>.
+ -->
+>>>>>>> b1ac62d (new commit)
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <EcosystemIcon />
     </template>
+<<<<<<< HEAD
     <template #heading>Third</template>
     Illustrate how your visual analytics approach can be used to identify new patterns 
     and/or anomalies that are present in the knowledge graph after you have added the links you deemed reliable in question 2. 
     Limit your response to 300 words and 4 images.
+=======
+    <template #heading>Third question</template>
+
+    Illustrate how your visual analytics approach can be used to identify new patterns and/or anomalies that are present in the knowledge graph after you have added the links you deemed reliable in question 2. 
+    Limit your response to 300 words and 4 images.
+    <!-- <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
+    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
+    you need more resources, we suggest paying
+    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
+    a visit.
+ -->
+>>>>>>> b1ac62d (new commit)
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
+<<<<<<< HEAD
     <template #heading>Fourth</template>
     Identify companies that fit a pattern of illegal fishing. 
     Use visualizations to support your conclusions and your confidence in them. 
@@ -70,16 +106,34 @@ import SupportIcon from './icons/IconSupport.vue'
     the official
     <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
     twitter account for latest news in the Vue world.-->
+=======
+    <template #heading>Fourth question</template>
+
+    Identify companies that fit a pattern of illegal fishing. 
+    Use visualizations to support your conclusions and your confidence in them. 
+    Limit your response to 300 words and 4 images.
+>>>>>>> b1ac62d (new commit)
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
+<<<<<<< HEAD
     <template #heading>Fifth</template>
+=======
+    <template #heading>Fifth question</template>
+>>>>>>> b1ac62d (new commit)
     Reflection: What was the most difficult aspect of working with this knowledge graph? 
     Did you have the tools and resources you needed to complete the challenge? 
     What additional resources would have helped you? 
     Limit your response to 300 words
+<<<<<<< HEAD
+=======
+    
+    <!--
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    -->
+>>>>>>> b1ac62d (new commit)
   </WelcomeItem>
 </template>
