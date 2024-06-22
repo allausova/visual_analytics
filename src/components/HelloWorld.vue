e8b2a0847e650d73eff5cb2aac91ef043e7ab1c9
Алла Усова <allausova@MBP-usova.wind3.hub>
2024-06-22 15:40:28 +0200

    initial creation of the template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      The country of Oceanus has sought FishEye International’s help in identifying companies possibly engaged in illegal, unreported, and unregulated (IUU) fishing. 
+      Using visual analytics, can you help FishEye identify companies that may be engaged in illegal fishing?
+
+      Please visit <a href="https://vast-challenge.github.io/2023/MC2.html" target="_blank" rel="noopener">VAST Challenge 2023: Mini-Challenge 2</a> 
+       for more information and to download the data.
+    
+      </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,85 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>First</template>
+
+    Use visual analytics to identify temporal patterns for individual entities and between entities in the knowledge graph FishEye created from trade records. 
+    Categorize the types of business relationship patterns you find. 
+    Limit your response to 600 words and 6 images.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Second</template>
+    Evaluate the sets of predicted knowledge graph links FishEye has provided using visual analytics. 
+    Which sets are most reliable for completing the graph? 
+    Limit your response to 600 words and 6 images.
+    <!--This project is served and bundled with
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
+    recommended IDE setup is
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
+    you need to test your components and web pages, check out
+    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+      >Cypress Component Testing</a
+    >.
+
+    <br />
+
+    More instructions are available in <code>README.md</code>.-->
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Third</template>
+    Illustrate how your visual analytics approach can be used to identify new patterns 
+    and/or anomalies that are present in the knowledge graph after you have added the links you deemed reliable in question 2. 
+    Limit your response to 300 words and 4 images.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Fourth</template>
+    Identify companies that fit a pattern of illegal fishing. 
+    Use visualizations to support your conclusions and your confidence in them. 
+    Limit your response to 300 words and 4 images.
+    <!--<a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
+    Discord server, or
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
+      >StackOverflow</a
+    >. You should also subscribe to
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
+    the official
+    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
+    twitter account for latest news in the Vue world.-->
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Fifth</template>
+    Reflection: What was the most difficult aspect of working with this knowledge graph? 
+    Did you have the tools and resources you needed to complete the challenge? 
+    What additional resources would have helped you? 
+    Limit your response to 300 words
+  </WelcomeItem>
+</template>
